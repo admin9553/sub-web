f4064cde2436333300c34d04fd8edbe1d16e3403
admin9553 <95000608+admin9553@users.noreply.github.com>
2021-11-27 10:25:27 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -255,7 +255,7 @@ export default {
           ClashR: "clashr",
           Surge2: "surge&ver=2",
         },
-        backendOptions: [{ value: "https://subweb-heroku.yunbai.workers.dev/sub?" }],
+        backendOptions: [{ value: "https://lucky-fog-0069.jin4333222288887163com.workers.dev/sub?" }],
         remoteConfig: [
         {
             label: "ACL4SSR",
